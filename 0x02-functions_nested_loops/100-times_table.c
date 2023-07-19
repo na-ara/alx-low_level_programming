486d5e03a07b66b59c113154b6e1fc94113cdbf0
na-ara <64538086+na-ara@users.noreply.github.com>
2023-07-18 21:22:44 -0700

    final

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,6 +9,7 @@ void print_times_table(int n)
 
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -16,6 +17,7 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
+
 				if (j == 0)
 				{
 					_putchar(k + '0');
