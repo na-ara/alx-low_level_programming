e413efade741a3078e3a5b5ee46335ed6659b870
“na-ara” <nana.taylor.hayford@gmail.com>
2023-07-20 18:35:47 -0700

    all

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,7 +16,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c )
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
 	{
 		largest = b;
 	}
@@ -25,5 +29,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-	return (largest);
+return (largest);
 }
